9d87d9092cdb13847259b436e10ecfbc187a488a
Aron Nicholasson <aronmar@live.no>
2023-04-27 23:03:56 +0200

    feat: Option to pass header object with key and value of header
    See https://github.com/alhazmy13/angular-csv-ext/commit/358047a518bc21d8d871bc31e3d19f926c0f6a80

--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,198 @@
-import {ConfigDefaults, CsvConfigConsts, DataItem, Options} from "./constants";
+import {ConfigDefaults, CsvConfigConst, DataItem, Options} from "./constants";
 
 declare global {
-  interface Navigator {
-    msSaveBlob: (blobOrBase64: Blob | string, filename: string) => void;
-  }
+	interface Navigator {
+		msSaveBlob: (blobOrBase64: Blob | string, filename: string) => void;
+	}
 }
 
 export class SimpleCsv {
-  public fileName: string | undefined;
-  public labels: Array<String> | undefined;
-  public data: DataItem[];
+	public fileName: string | undefined;
+	public labels: Array<String> | undefined;
+	public data: DataItem[];
 
-  private readonly _options: Options;
-  private csv = "";
+	private readonly _options: Options;
+	private csv = "";
 
-  constructor(DataJSON: DataItem[], filename: string, options?: Partial<Options>) {
-    let config = options || {};
+	constructor(DataJSON: DataItem[], filename: string, options?: Partial<Options>) {
+		let config = options || {};
 
-    this.data = typeof DataJSON != "object" ? JSON.parse(DataJSON) : DataJSON;
+		this.data = typeof DataJSON != "object" ? JSON.parse(DataJSON) : DataJSON;
 
-    this._options = objectAssign({}, ConfigDefaults, config);
+		this._options = objectAssign({}, ConfigDefaults, config);
 
-    if (this._options.filename) {
-      this._options.filename = filename;
-    }
+		if (this._options.filename) {
+			this._options.filename = filename;
+		}
 
-    this.generateCsv();
-  }
+		this.generateCsv();
+	}
 
-  /**
-   * Generate and Download Csv
-   */
-  private generateCsv(): any {
-    if (this._options.useBom) {
-      this.csv += CsvConfigConsts.BOM;
-    }
+	/**
+	 * Generate and Download Csv
+	 */
+	private generateCsv(): any {
+		if (this._options.useBom) {
+			this.csv += CsvConfigConst.BOM;
+		}
 
-    if (this._options.showTitle) {
-      this.csv += this._options.title + "\r\n\n";
-    }
+		if (this._options.showTitle) {
+			this.csv += this._options.title + "\r\n\n";
+		}
 
-    this.getHeaders();
-    this.getBody();
+		if (this._options.useObjHeader && Object.keys(this._options.objHeader).length > 0) {
+			this.getHeaderFromObj();
+			this.getBodyAccordingHeader();
+		} else {
+			this.getHeaders();
+			this.getBody();
+		}
 
-    if (this.csv == "") {
-      console.log("Invalid data");
-      return;
-    }
+		if (this.csv == "") {
+			console.log("Invalid data");
+			return;
+		}
 
-    if (this._options.noDownload) {
-      return this.csv;
-    }
+		if (this._options.noDownload) {
+			return this.csv;
+		}
 
-    let blob: Blob = new Blob([this.csv], { type: "text/csv;charset=utf8;" });
+		let blob: Blob = new Blob([this.csv], {type: "text/csv;charset=utf8;"});
 
-    if (navigator.msSaveBlob) {
-      let filename = this._options.filename.replace(/ /g, "_") + ".csv";
-      navigator.msSaveBlob(blob, filename);
-    } else {
-      let link = document.createElement("a");
+		if (navigator.msSaveBlob) {
+			let filename = this._options.filename.replace(/ /g, "_") + ".csv";
+			navigator.msSaveBlob(blob, filename);
+		} else {
+			let link = document.createElement("a");
 
-      // @ts-ignore
-      link.href = URL.createObjectURL(blob);
+			// @ts-ignore
+			link.href = URL.createObjectURL(blob);
 
-      link.setAttribute("target", "_blank");
-      link.setAttribute("visibility", "hidden");
-      link.download = this._options.filename.replace(/ /g, "_") + ".csv";
+			link.setAttribute("target", "_blank");
+			link.setAttribute("visibility", "hidden");
+			link.download = this._options.filename.replace(/ /g, "_") + ".csv";
 
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
+	}
 
-  /**
-   * Create Headers
-   */
-  getHeaders(): void {
-    if (this._options.headers.length > 0) {
-      const { headers } = this._options;
-      let row = headers.reduce((headerRow, header) => {
-        return headerRow + header + this._options.fieldSeparator;
-      }, "");
-      row = row.slice(0, -1);
-      this.csv += row + CsvConfigConsts.EOL;
-    }
-  }
+	/**
+	 * Create Headers
+	 */
+	getHeaders(): void {
+		if (this._options.headers.length > 0) {
+			const {headers} = this._options;
+			let row = headers.reduce((headerRow, header) => {
+				return headerRow + header + this._options.fieldSeparator;
+			}, "");
+			row = row.slice(0, -1);
+			this.csv += row + CsvConfigConst.EOL;
+		}
+	}
 
-  /**
-   * Create Body
-   */
-  getBody() {
-    for (let i = 0; i < this.data.length; i++) {
-      let row = "";
-      if (this._options.useHeader && this._options.headers.length > 0) {
-        for (const index of this._options.headers) {
-          row +=
-            this.formatData(this.data[i][index]) + this._options.fieldSeparator;
-        }
-      } else {
-        for (const index in this.data[i]) {
-          row +=
-            this.formatData(this.data[i][index]) + this._options.fieldSeparator;
-        }
-      }
-      row = row.slice(0, -1);
-      this.csv += row + CsvConfigConsts.EOL;
-    }
-  }
+	/**
+	 * Create Header from Object
+	 */
+	getHeaderFromObj(): void {
+		if (Object.keys(this._options.objHeader).length > 0) {
+			let row = '';
+			Object.keys(this._options.objHeader).forEach(key => {
+				row += this._options.objHeader[key] + this._options.fieldSeparator;
+			})
+			row = row.slice(0, -1);
+			this.csv += row + CsvConfigConst.EOL;
+		}
+	}
 
-  /**
-   * Format Data
-   * @param {any} data
-   */
-  formatData(data: any) {
-    if (
-      this._options.decimalSeparator === "locale" &&
-      SimpleCsv.isFloat(data)
-    ) {
-      return data.toLocaleString();
-    }
+	/**
+	 * Create Body according to obj header
+	 */
+	getBodyAccordingHeader(): void {
+		for (let i = 0; i < this.data.length; i++) {
+			let row = "";
+			if (this._options.useObjHeader && Object.keys(this._options.objHeader).length > 0) {
+				Object.keys(this._options.objHeader).forEach(key => {
+					row += this.formatData(this.data[i][key]) + this._options.fieldSeparator;
+				})
+			}
+			row = row.slice(0, -1);
+			this.csv += row + CsvConfigConst.EOL;
+		}
+	}
 
-    if (this._options.decimalSeparator !== "." && SimpleCsv.isFloat(data)) {
-      return data.toString().replace(".", this._options.decimalSeparator);
-    }
+	/**
+	 * Create Body
+	 */
+	getBody() {
+		for (let i = 0; i < this.data.length; i++) {
+			let row = "";
+			if (this._options.useHeader && this._options.headers.length > 0) {
+				for (const index of this._options.headers) {
+					row +=
+						this.formatData(this.data[i][index]) + this._options.fieldSeparator;
+				}
+			} else {
+				for (const index in this.data[i]) {
+					row +=
+						this.formatData(this.data[i][index]) + this._options.fieldSeparator;
+				}
+			}
+			row = row.slice(0, -1);
+			this.csv += row + CsvConfigConst.EOL;
+		}
+	}
 
-    if (typeof data === "string") {
-      data = data.replace(/"/g, '""');
-      if (
-        this._options.quoteStrings ||
-        data.indexOf(",") > -1 ||
-        data.indexOf("\n") > -1 ||
-        data.indexOf("\r") > -1
-      ) {
-        data = this._options.quoteStrings + data + this._options.quoteStrings;
-      }
-      return data;
-    }
+	/**
+	 * Format Data
+	 * @param {any} data
+	 */
+	formatData(data: any) {
+		if (
+			this._options.decimalSeparator === "locale" &&
+			SimpleCsv.isFloat(data)
+		) {
+			return data.toLocaleString();
+		}
 
-    if (this._options.nullToEmptyString) {
-      if (data === null) {
-        return (data = "");
-      }
-      return data;
-    }
+		if (this._options.decimalSeparator !== "." && SimpleCsv.isFloat(data)) {
+			return data.toString().replace(".", this._options.decimalSeparator);
+		}
 
-    if (typeof data === "boolean") {
-      return data ? "TRUE" : "FALSE";
-    }
-    return data;
-  }
+		if (typeof data === "string") {
+			data = data.replace(/"/g, '""');
+			if (
+				this._options.quoteStrings ||
+				data.indexOf(",") > -1 ||
+				data.indexOf("\n") > -1 ||
+				data.indexOf("\r") > -1
+			) {
+				data = this._options.quoteStrings + data + this._options.quoteStrings;
+			}
+			return data;
+		}
 
-  getCsvData() {
-    return this.csv;
-  }
+		if (this._options.nullToEmptyString) {
+			if (data === null) {
+				return ("");
+			}
+			return data;
+		}
 
-  /**
-   * Check if is Float
-   * @param {any} input
-   */
-  static isFloat(input: any) {
-    return +input === input && (!isFinite(input) || Boolean(input % 1));
-  }
+		if (typeof data === "boolean") {
+			return data ? "TRUE" : "FALSE";
+		}
+		return data;
+	}
+
+	/**
+	 * Check if is Float
+	 * @param {any} input
+	 */
+	static isFloat(input: any) {
+		return +input === input && (!isFinite(input) || Boolean(input % 1));
+	}
 }
 
 let hasOwnProperty = Object.prototype.hasOwnProperty;
@@ -172,12 +203,12 @@ let propIsEnumerable = Object.prototype.propertyIsEnumerable;
  * @param {any} val
  */
 function toObject(val: any) {
-  if (val === null || val === undefined) {
-    throw new TypeError(
-      "Object.assign cannot be called with null or undefined"
-    );
-  }
-  return Object(val);
+	if (val === null || val === undefined) {
+		throw new TypeError(
+			"Object.assign cannot be called with null or undefined"
+		);
+	}
+	return Object(val);
 }
 
 /**
@@ -186,27 +217,27 @@ function toObject(val: any) {
  * @param {any[]} _source
  */
 function objectAssign(target: any, ..._source: any[]) {
-  let from: any;
-  let to = toObject(target);
-  let symbols: any;
+	let from: any;
+	let to = toObject(target);
+	let symbols: any;
 
-  for (let s = 1; s < arguments.length; s++) {
-    from = Object(arguments[s]);
+	for (let s = 1; s < arguments.length; s++) {
+		from = Object(arguments[s]);
 
-    for (const key in from) {
-      if (hasOwnProperty.call(from, key)) {
-        to[key] = from[key];
-      }
-    }
+		for (const key in from) {
+			if (hasOwnProperty.call(from, key)) {
+				to[key] = from[key];
+			}
+		}
 
-    if ((<any>Object).getOwnPropertySymbols) {
-      symbols = (<any>Object).getOwnPropertySymbols(from);
-      for (let i = 0; i < symbols.length; i++) {
-        if (propIsEnumerable.call(from, symbols[i])) {
-          to[symbols[i]] = from[symbols[i]];
-        }
-      }
-    }
-  }
-  return to;
+		if ((<any>Object).getOwnPropertySymbols) {
+			symbols = (<any>Object).getOwnPropertySymbols(from);
+			for (let i = 0; i < symbols.length; i++) {
+				if (propIsEnumerable.call(from, symbols[i])) {
+					to[symbols[i]] = from[symbols[i]];
+				}
+			}
+		}
+	}
+	return to;
 }
